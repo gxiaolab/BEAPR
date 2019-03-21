794ac66397e4b9753dd3fa2cd5f6af7316da0cb8
eiwenyang089 <yyang027@ucr.edu>
2019-03-20 18:11:56 -0700

    Add files via upload

--- Simulation/run_beapr.sh
+++ Simulation/run_beapr.sh
@@ -1,10 +1,11 @@
 ./genMean 10 0.8
 Rscript Sim.r data/mean 2 10
-Rscript Reg.r reg.rc data/mean.out data/asbclip.in
-Rscript Pred.r data/asbclip.in data/asbclip.out 2
-Rscript chi.r data/mean.out data/chi.out
-Rscript fisher.r data/mean.out data/fisher.out
-Rscript pROC_new.r data/asbclip.out
-Rscript pROC_new.r data/chi.out
-Rscript pROC_new.r data/fisher.out
-Rscript PRROC.r data/asbclip.out  data/chi.out data/fisher.out
+# The following command lines are for reproducing results on simulated data without cross-linking bias 
+#Rscript Reg.r reg.rc data/mean.out data/asbclip.in
+#Rscript Pred.r data/asbclip.in data/asbclip.out 2
+#Rscript chi.r data/mean.out data/chi.out
+#Rscript fisher.r data/mean.out data/fisher.out
+#Rscript pROC_new.r data/asbclip.out
+#Rscript pROC_new.r data/chi.out
+#Rscript pROC_new.r data/fisher.out
+#Rscript PRROC.r data/asbclip.out  data/chi.out data/fisher.out
