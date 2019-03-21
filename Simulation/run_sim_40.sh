e91c265d73d7f78b0d0cbfb5d3b3114864ad0997
eiwenyang089 <yyang027@ucr.edu>
2019-03-20 18:08:20 -0700

    Delete run_sim_40.sh

--- Simulation/run_sim_40.sh
+++
@@ -1,7 +0,0 @@
-./test
-Rscript Sim.r /home/yyang/data/eclip/K562/SRSF1_test/sim/mean 2 40
-Rscript Reg.r /home/yyang/data/eclip/K562/SRSF1_test/sim/reg.rc /home/yyang/data/eclip/K562/SRSF1_test/sim/mean.out /home/yyang/data/eclip/K562/SRSF1_test/sim/asbclip.in
-Rscript Pred.r /home/yyang/data/eclip/K562/SRSF1_test/sim/asbclip.in /home/yyang/data/eclip/K562/SRSF1_test/sim/asbclip.out 2
-Rscript chi.r /home/yyang/data/eclip/K562/SRSF1_test/sim/mean.out /home/yyang/data/eclip/K562/SRSF1_test/sim/chi.out
-Rscript pROC.r /home/yyang/data/eclip/K562/SRSF1_test/sim/asbclip.out
-Rscript pROC.r /home/yyang/data/eclip/K562/SRSF1_test/sim/chi.out
